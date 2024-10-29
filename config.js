93461a393366db2f38cde7422cfeb81b9af69918
Fikhri85 <149870739+Fikhri85@users.noreply.github.com>
2024-10-29 20:58:58 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {} 
+
+
+// your telegram API credentials
+// please go to https://my.telegram.org to create your API credentials
+config.telegramApiId = 24871034;
+config.telegramApiHash = '4e1c9350739031fe072a1d30473aecec';
+
+
+// server settings
+config.hostname = '127.0.0.1';
+config.port = 3000;
+
+
+module.exports = config;
